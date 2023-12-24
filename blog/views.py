9a24bb9024e55c0d203c89a203fa24a99664054c
nck4s <nn.2nn@mail.ru>
2023-12-24 15:56:48 +1000

    model, db

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Nika',
-        'title': 'Blog post 1',
-        'content': 'This is my blog',
-        'date_posted': '5th March, 2021', 
-    },
-    {
-        'author': 'John',
-        'title': 'Blog post 2',
-        'content': 'This is his blog post',
-        'date_posted': '9th March, 2022', 
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About page'})
